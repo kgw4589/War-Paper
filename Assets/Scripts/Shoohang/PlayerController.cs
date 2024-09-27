080ded48c81e63f45750351e211d3294fe3e1e4f
Yoimiya <kgw4589@gmail.com>
2024-09-28 00:07:55 +0900

    Feat :: 플레이어 총알 Trail 랜더러 추가

--- Assets/Scripts/Shoohang/PlayerController.cs
+++ Assets/Scripts/Shoohang/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : BasicPlane, IDamagable
     private float _rotXAmount = 7.5f;
     private float _rotZAmount = 7.5f;
 
+    private float _currentFireTime = 0.0f;
+    private float _fireDelay = 1.0f;
+
     private void Update()
     {
         RotXValue = Input.GetAxis("Mouse Y") * _rotXAmount;
@@ -18,7 +21,14 @@ public class PlayerController : BasicPlane, IDamagable
 
     private void Fire()
     {
-        if (Input.GetButtonDown("Fire1"))
+        _currentFireTime += Time.deltaTime;
+
+        if (_currentFireTime < _fireDelay)
+        {
+            return;
+        }
+        
+        if (Input.GetButton("Fire1"))
         {
             GameObject bullet = ObjectPoolManager.Instance.GetBullet();
 
@@ -27,6 +37,8 @@ public class PlayerController : BasicPlane, IDamagable
                 bullet.transform.position = transform.position;
                 bullet.transform.rotation = transform.rotation;
             }
+
+            _currentFireTime = 0;
         }
     }
 
